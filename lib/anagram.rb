1bcc3b04c5fbc6211a5bd3cb0e701e5d7a4cc737
ghemsley <ghemsley@protonmail.ch>
2020-11-03 11:40:11 -0600

    commit tree 21808f9124c3c499ba09397c2f50b64b05b2cbb1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  def initialize(word = '')
+    @word = word
+  end
+
+  attr_accessor :word
+
+  def match(word_array)
+    match_array = @word.split('')
+    match_array.sort!
+    final_array = []
+    word_array.each do |word|
+      test_array = word.split('')
+      test_array.sort!
+      final_array.push(word) if test_array == match_array
+    end
+    final_array
+  end
+end
